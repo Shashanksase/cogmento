648d4e6e6bbac8ba4e79a9ea66b7a14daf5da422
shashanksase <shashank.sase@gmail.com>
2020-03-28 14:45:14 +0530

    java project

--- src/main/java/com/cogemnto/configurations/configurations.java
+++ src/main/java/com/cogemnto/configurations/configurations.java
@@ -1,11 +1,10 @@
 package com.cogemnto.configurations;
 
 public class configurations {
-	
-	public static void main(String[] args) {
-		System.out.println();
-	}
-	
 
+	// Environment/Global variables
+	public final static String url = "https://freecrm.co.in/";
+	public final static String browserPath = System.getProperty("user.dir")
+			+ "\\src\\main\\resources\\Browserdrivers\\";
+	public final static long implicitWait = 5000;
 }
-
