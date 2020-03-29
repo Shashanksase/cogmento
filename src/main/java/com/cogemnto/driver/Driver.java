cd2ee6bdfdf08d71dffe04d85332b5aefc5bb3fd
shashanksase <shashank.sase@gmail.com>
2020-03-29 10:17:59 +0530

    29/03/2020

--- src/main/java/com/cogemnto/driver/Driver.java
+++ src/main/java/com/cogemnto/driver/Driver.java
@@ -1,10 +1,22 @@
 package com.cogemnto.driver;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+
+import com.cogemnto.configurations.configurations;
+
 public class Driver {
+	public WebDriver dr;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void initialize(String browser) {
 
+		if (browser.equalsIgnoreCase("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", configurations.browserPath + "chromedriver.exe");
+			dr.manage().timeouts().implicitlyWait(configurations.implicitWait, TimeUnit.SECONDS);
+			dr.get(configurations.url);
+
+		}
+		dr.manage().window().maximize();
 	}
-
 }
---
+++ src/main/java/com/cogemnto/pages/PageHost.java
@@ -0,0 +1,35 @@
+package com.cogemnto.pageshost;
+
+import org.openqa.selenium.WebDriver;
+
+import com.cogemnto.pages.calls.Calls;
+import com.cogemnto.pages.campaigns.Campaigns;
+import com.cogemnto.pages.cases.Cases;
+import com.cogemnto.pages.documents.Documents;
+import com.cogemnto.pages.forms.Forms;
+
+public class PageHost {
+
+	
+	public Cases cases;
+	public Calls calls;
+	public Documents documents;
+	public Campaigns campaigns;
+	public Forms forms;
+	
+
+	// add object for truck,motorcycle and camper
+
+	public PageHost(WebDriver dr)
+	{
+		cases =new Cases(dr);
+		calls= new Calls(dr);
+		documents= new Documents(dr);
+		campaigns = new Campaigns(dr);
+		forms =new Forms(dr);
+		
+		//initialize the reference
+	
+	}
+
+}
